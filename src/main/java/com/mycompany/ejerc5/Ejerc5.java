52f633ba85dab1f3a9711982a4d478a2236d65bb
David R.L <David R.L@LAPTOP-1842CCOD>
2023-02-25 16:02:47 -0500

    Sisecambio

---
+++ src/main/java/com/mycompany/ejerc5/Ejerc5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerc5;
+
+/**
+ *
+ * @author David R.L
+ */
+import java.util.*;
+public class Ejerc5 {
+
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        int num=entrada.nextInt();
+        System.out.println("El cuadrado del numero es: "+(Math.pow(num, 2)));
+        System.out.println("El cubo del numero es: "+(Math.pow(num,3)));
+    }
+}
